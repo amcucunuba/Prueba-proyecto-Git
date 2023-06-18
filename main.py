a3609a1130ef68248dd1ee0d2e79e17bf8f9d529
leandres <leandres1985@gmail.com>
2023-06-18 19:26:39 +0200

    primer commit de andres en brazo1 andres

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ print('Hola Mundo!')
 print("linea 2")
 print("Segunda linea creada por Andres")
 print("2da linea de Angela")
-suma
+
 
 suma = 2 + 5
 print (suma)
 
 print("Tercera linea creada por andres. Aún en el main branch")
-main
+
+# multiplicacion
+a=3
+b=5
+resultado = a*b
+print(resultado)
